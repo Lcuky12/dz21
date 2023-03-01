e7dc0b1e92553d9f9fb851c699ae854afea96ee5
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-01 12:18:33 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userEnters;
+            bool examination = true;
+         
+            while (examination) 
+            {
+                Console.WriteLine("Введите сообщение");
+                userEnters= Console.ReadLine();
+
+                if (userEnters == "exit") 
+                {
+                    break;
+                } 
+            }
+        }
+    }
+}
